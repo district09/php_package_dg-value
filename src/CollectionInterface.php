b899792a2dd491eee31060e3b5c1c9f498a12fc7
Peter Decuyper <peter.decuyper@digipolis.gent>
2018-10-26 15:16:35 +0200

    Added the CollectionInterface and abstract implementation.

---
+++ src/CollectionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DigipolisGent\Value;
+
+/**
+ * Interface for the base ValueCollection object.
+ *
+ * All collections must implement this interface.
+ *
+ * @package DigipolisGent\Value
+ */
+interface CollectionInterface extends \IteratorAggregate, ValueInterface
+{
+
+}
